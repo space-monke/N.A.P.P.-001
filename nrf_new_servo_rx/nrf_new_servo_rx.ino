93dcd733688d093896deab9383da9e891f0ab571
luka <lukabavshvi@gmail.com>
2023-01-29 16:28:32 +0400

    formatting the right way!
    tabs and indents

--- nrf_new_servo_rx/nrf_new_servo_rx.ino
+++ nrf_new_servo_rx/nrf_new_servo_rx.ino
@@ -4,7 +4,7 @@
 #include <Servo.h>
 
 RF24 radio(7, 8); // CE, CSN
-const byte addresses[][6] = {"00001"};
+const byte addresses[][6]={"00001"};
 
 Servo servo1;
 Servo servo2;
@@ -13,49 +13,47 @@ Servo mainEngine;
 
 struct dato
 {
-  int x;
-  int y;
-  int throttle;
-  bool forceStop;
+	int x;
+	int y;
+	int throttle;
+	bool forceStop;
 };
 
-void setup() {
-  servo1.attach(9);
-  servo2.attach(6);
-  tailServo.attach(5);
-  mainEngine.attach(3);
-  Serial.begin(9600);
-  radio.begin();
-  radio.openReadingPipe(0, addresses[0]);
-  radio.setPALevel(RF24_PA_MAX);
+void setup()
+{
+	servo1.attach(9);
+	servo2.attach(6);
+	tailServo.attach(5);
+	mainEngine.attach(3);
+	Serial.begin(9600);
+	radio.begin();
+	radio.openReadingPipe(0, addresses[0]);
+	radio.setPALevel(RF24_PA_MAX);
 }
 
-void loop() {
-  delay(5);
-  
-  radio.startListening();
-  if ( radio.available()) {
-    while (radio.available()) {
-      
-      dato recievedData;
-      
-      radio.read(&recievedData, sizeof(recievedData));
-      recievedData.x = map(recievedData.x, 0, 1024, 150, 30);
-      int s2 = map(recievedData.y, 0, 1024, 45, 135);
-      int s1 = map(recievedData.y, 0, 1024, 135, 45);
-      recievedData.throttle = map(recievedData.throttle, 0,1024, 0, 160);
-      
-      Serial.println(recievedData.throttle);
-      
-      if(recievedData.forceStop)
-      {
-        mainEngine.write(0);
-      }
-      
-      servo1.write(s1);
-      servo2.write(s2);
-      tailServo.write(recievedData.x);
-      mainEngine.write(recievedData.throttle);
-    }
-  }
+void loop()
+{
+	delay(5);
+	radio.startListening();
+	if(radio.available())
+	{
+		while(radio.available())
+		{
+			dato recievedData;
+			radio.read(&recievedData, sizeof(recievedData));
+			recievedData.x = map(recievedData.x, 0, 1024, 150, 30);
+			int s2 = map(recievedData.y, 0, 1024, 45, 135);
+			int s1 = map(recievedData.y, 0, 1024, 135, 45);
+			recievedData.throttle = map(recievedData.throttle, 0,1024, 0, 160);
+			Serial.println(recievedData.throttle);
+			if(recievedData.forceStop)
+			{
+				mainEngine.write(0);
+			}
+			servo1.write(s1);
+			servo2.write(s2);
+			tailServo.write(recievedData.x);
+			mainEngine.write(recievedData.throttle);
+		}
+	}
 }
--- nrf_new_servo_tx/nrf_new_servo_tx.ino
+++ nrf_new_servo_tx/nrf_new_servo_tx.ino
@@ -7,39 +7,37 @@ const byte addresses[][6] = {"00001"};
 
 struct dato
 {
-  int x;
-  int y;
-  int throttle;
-  bool forceStop = false;
+	int x;
+	int y;
+	int throttle;
+	bool forceStop = false;
 };
 bool getButtonState(int num)
 {
-  return digitalRead(num);
+	return digitalRead(num);
 }
 
-void setup() {
-  pinMode(2, INPUT);
-  Serial.begin(9600);
-  radio.begin();
-  radio.openWritingPipe(addresses[0]);
-  radio.setPALevel(RF24_PA_MAX);
+void setup()
+{
+	pinMode(2, INPUT);
+	Serial.begin(9600);
+	radio.begin();
+	radio.openWritingPipe(addresses[0]);
+	radio.setPALevel(RF24_PA_MAX);
 }
 
-void loop() {
-  delay(5);
-
-  dato dato2;
-
-  bool forceStop = getButtonState(4);
-  int analogOne = analogRead(A0);
-  int analogTwo = analogRead(A1);
-  int throttle = analogRead(A2);
-  radio.stopListening();
-
-  dato2.x = analogOne;
-  dato2.y = analogTwo;
-  dato2.throttle = throttle;
-
-  Serial.println(dato2.throttle);
-  radio.write(&dato2, sizeof(dato2));
+void loop()
+{
+	delay(5);
+	dato dato2;
+	bool forceStop = getButtonState(4);
+	int analogOne = analogRead(A0);
+	int analogTwo = analogRead(A1);
+	int throttle = analogRead(A2);
+	radio.stopListening();
+	dato2.x = analogOne;
+	dato2.y = analogTwo;
+	dato2.throttle = throttle;
+	Serial.println(dato2.throttle);
+	radio.write(&dato2, sizeof(dato2));
 }
